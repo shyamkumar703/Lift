b435bc721da7e4527e2449c8ac6bde28fe5a1d52
Shyam Kumar <shyamkumar@berkeley.edu>
2021-12-24 21:41:27 -0800

    Initial Commit

---
+++ Lift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lift
+//
+//  Created by Shyam Kumar on 12/24/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
